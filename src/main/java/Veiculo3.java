55a5226bc9f94da7ad96a9d9292f120b9fdcc8b6
Paulo <paulo.moraes@sou.unifal-mg.edu.br>
2023-05-06 14:40:39 -0300

    Finalizando a ultima versão da transportadora

--- src/main/java/Veiculo3.java
+++ src/main/java/Veiculo3.java
@@ -3,10 +3,17 @@ public class Veiculo3 {
         private double carga;
         private double cargaMaxima;
 
+        /*
+        Primeira mudança dessa versão para a anterior, aqui desejamos calcular os pesos em Newton das cargas
+        Então para o metodo abaixo ao inves de apenas adicionarmos a carga maxima
+        Chamamos um metodo que converte o peso da cargaMaxima de kg para Newtons
+         */
         public Veiculo2(double cargaMaxima) {
             this.cargaMaxima = quilosParaNewtons(cargaMaxima);
         }
-
+/*
+O mesmo é valido para esse metodo, você converte a carga em kg para newtons
+ */
         public double getCarga() {
             return newtonsParaQuilos(carga);
         }
@@ -30,11 +37,15 @@ public class Veiculo3 {
             this.carga+= pesoEmNewtons;
             return true;
         }
-
+/*
+Esse metodo é responsavel por converter Newton para Kg
+ */
         private double newtonsParaQuilos(double peso) {
             return peso / 9.8;
         }
-
+/*
+Metodo responsavel por converter KG para Newtons
+ */
         private double quilosParaNewtons(double peso) {
             return peso * 9.8;
         }
